7777c4416251b63d21e13eb9295862a6fa9d5526
massam zohaib <zoahibmassam40@gmail.com>
2023-09-30 16:31:53 +0200

    added make deb

---
+++ make_deb.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+defaultPath="opt/massam/service/"
+foldername="softetherVpn_Installation"
+user="orangepi"
+DEB_NAME="softetherVpn"
+deb_info_file="control"
+description="This service is used to install and uninstall softetherVpn on the device"
+version="1"
+
+function copyFiles {
+
+    search="*"
+    for file in $search
+    do
+       
+        if [[ "$file" == *"."* ]] && [[ "$file" != "make_deb.sh" ]];
+        then 
+            #copy file to the path
+            sudo cp "$file" "$DEB_NAME/$defaultPath$foldername"
+
+        fi
+    done
+
+}
+
+function myLocation {
+    location="$(pwd)"
+    echo -e "Directory: $location"
+}
+
+#creation of a folder
+mkdir $DEB_NAME && cd $DEB_NAME
+myLocation
+
+#creation of DEBIAN folder
+mkdir "DEBIAN" && cd DEBIAN
+myLocation
+
+echo "Package: $DEB_NAME" >> $deb_info_file
+echo "Version: $version" >> $deb_info_file
+echo "Maintainer: $user" >> $deb_info_file
+echo "Architecture: all" >> $deb_info_file
+echo "Description: $description" >> $deb_info_file
+
+
+cd ..
+myLocation
+#creation of the defaultPath
+
+mkdir -p "$defaultPath$foldername"
+
+# sudo chown -R $user:$user "$defaultPath$foldername"
+
+cd ..
+myLocation
+
+copyFiles
+
+
+#build the deb package
+sleep 1
+sudo dpkg --build "$DEB_NAME"
+sudo rm -r "$DEB_NAME"
+
+echo "DEB CREATED SUCCESSFULLY"
